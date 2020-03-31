989fe9764c8ff1cea344633cc55bbe2f22adf5fa
Reuben Remy <reubenremy@gmail.com>
2020-03-31 11:51:42 -0400

    Added Morph Styles

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Issue from './components/Issue';
 import IssueList from './components/IssueList';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
 
 function App() {
   return (
--- src/components/issueList.jsx
+++ src/components/issueList.jsx
@@ -4,14 +4,8 @@ import Issue from './Issue';
 import { loadData } from "../utils/loadData";
 import Styled from 'styled-components';
 
-const List = Styled.ul`
-    list-style: none;
-`
-
 const IssueItem = Styled.li`
-    background: #eee;
-    box-shadow: 2px 2px 5px #ccc;
-    margin: 15px;
+    margin: 20px;
     padding: 25px;
 `
 
@@ -34,17 +28,18 @@ class IssueList extends Component {
         const { issues } = this.state;
 
         return (
-            <List>
+            <ul>
                 {issues.map(issue => (
-                    <IssueItem key={issue.id}>
-                        {issue.title}
-                        <br />
-                        <Link to={`/issue/${issue.number}`}>
-                            See Details
-                        </Link>
-                    </IssueItem>
+                    <Link to={`/issue/${issue.number}`}>
+                        <IssueItem 
+                            key={issue.id}
+                            className="morph-active"
+                        >
+                            {issue.title}
+                        </IssueItem>
+                    </Link>
                 ))}
-            </List>
+            </ul>
         )
     }
 }
